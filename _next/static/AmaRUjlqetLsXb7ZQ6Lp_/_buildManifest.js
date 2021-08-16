2073b3ffe197e2321f3c239b9fab4f25c2fc0d13
Zak Holt <zak.j.holt@gmail.com>
2021-08-16 13:19:31 -0400

    Initial dist subtree commit

---
+++ _next/static/AmaRUjlqetLsXb7ZQ6Lp_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-822b2f94e1458c644b30.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1ce957b602fb371961ce.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
