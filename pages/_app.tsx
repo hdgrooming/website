656e445c380da55c19b84f19b11f5fd3d73a8b8b
Zak Holt <zak.j.holt@gmail.com>
2021-12-01 12:33:32 -0500

    serve fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import Head from "next/head";
 import "normalize.css";
 
-import "../theme/fonts.css";
-
 const MyApp = ({ Component, pageProps }: any) => {
   return (
     <>
       <Head>
         <title>Happy Dog Grooming Salon</title>
+        <link href="/fonts/fonts.css" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
