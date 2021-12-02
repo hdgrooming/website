0778456862e98b1752e4555ee52530e902d8a584
Zak Holt <zak.j.holt@gmail.com>
2021-12-01 20:05:27 -0500

    fix css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import "normalize.css";
 
+import { GlobalStyles } from "../components/GlobalStyles";
+
 const MyApp = ({ Component, pageProps }: any) => {
   return (
     <>
@@ -8,6 +10,8 @@ const MyApp = ({ Component, pageProps }: any) => {
         <title>Happy Dog Grooming Salon</title>
         <link href="/fonts/fonts.css" rel="stylesheet" />
       </Head>
+      <GlobalStyles />
+
       <Component {...pageProps} />
     </>
   );
